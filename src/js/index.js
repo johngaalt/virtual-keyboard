a029c7b15154cfba093c7b1831efc90e03975035
johngaalt <rajon777666@gmail.com>
2023-04-28 17:14:12 +0300

    feat: special symbols

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
+import keys from '../constants/keys';
 import '../styles/styles.scss';
+
+const keysElements = keys.map((key) => {
+  const div = document.createElement('span');
+  div.textContent = key.primary;
+  return div;
+});
+
+document.body.append(...keysElements);
